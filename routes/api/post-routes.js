64eb37a79e92b917722fb8dc3568d5a91b1600e3
JoDamaso <darryldamaso@gmail.com>
2022-03-20 19:54:39 -0500

    vote endpoints

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 
-// get all users
+// GET all posts with this end point
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
       // Query configuration
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -22,12 +23,13 @@ router.get('/', (req, res) => {
         });
 });
 
+// gets post by id
 router.get('/:id', (req, res) => {
     Post.findOne({
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         include: [
             {
             model: User,
@@ -48,6 +50,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// POST, lets you create POST with the title, post_url, user_id
 router.post('/', (req, res) => {
     Post.create({
         title: req.body.title,
@@ -61,6 +64,36 @@ router.post('/', (req, res) => {
         });
 });
 
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(() => {
+        // then find the post we just voted on
+        return Post.findOne({
+            where: {
+                id: req.body.post_id
+            },
+            attributes: [
+                'id',
+                'post_url',
+                'title',
+                'created_at',
+                // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+                [
+                sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                'vote_count'
+                ]
+            ]
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => res.json(err));
+    });
+});
+
+// PUT, a post is able to by updated from the title, with the given id param
 router.put('/:id', (req, res) => {
     Post.update(
         {
@@ -85,6 +118,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// DELETE a post from the given id param
 router.delete('/:id', (req, res) => {
     Post.destroy({
         where: {
